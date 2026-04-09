66c112ca6956a7614c992e6d6b783a7cb3891df4
Yevhenii Shpylovyi <yevheniishpylovyi398@gmail.com>
2026-04-09 18:21:24 +0300

    HW12

---
+++ Source/study/MyBTTaskTest.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "MyBTTaskTest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUDY_API UMyBTTaskTest : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
